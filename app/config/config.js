b484d475b788c2d5fb137819a974fa93d9dd0140
raihanjs <rayhangazi1024@gmail.com>
2025-01-11 11:29:35 +0600

    Project file structured and project package installed

---
+++ app/config/config.js
@@ -0,0 +1,21 @@
+export const PORT = 5000;
+
+export const DATABASE =
+  "mongodb+srv://admin:Admin905@cluster0.bhifpq3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+export const JWT_KEY = "";
+export const JWT_EXPIRE_TIME = 60 * 60 * 24 * 30;
+
+export const EMAIL_HOST = "mail.teamrabbil.com";
+export const EMAIL_PORT = 25;
+export const EMAIL_SECURITY = false;
+export const EMAIL_USER = "info@teamrabbil.com";
+export const EMAIL_PASSWORD = "~sR4[bhaC[Qs";
+export const EMAIL_UN_AUTH = false;
+
+export const WEB_CACHE = false;
+export const MAX_JSON_SIZE = "10mb";
+export const URL_ENCODED = true;
+
+export const REQUEST_LIMIT_TIME = 15 * 60 * 100; // 15 min
+export const REQUEST_LIMIT_NUMBER = 2000; // per 15 min 2000 req allowed
---
+++ readme.md
@@ -0,0 +1,21 @@
+## Task Manager API TODO list
+
+1. Project File Structure -- Done
+2. Project Package -- Done
+3. Route Config
+4. App.js Config
+5. PostMan Initial Config
+6. Email Credential Config
+7. Write Model Properties
+8. Registration API
+9. Login API
+10. ProfileDetails API
+11. ProfileUpdate API
+12. Email Verify API
+13. Code Verify API
+14. RESET Password API
+15. Create Task API
+16. Update Task API
+17. Delete Task API
+18. Task List By Status API
+19. Count Task API
